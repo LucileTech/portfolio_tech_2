824e24c3f3aa803cb99561e51dd2349ea2c09afe
LucileTech <lucile.t.tech@gmail.com>
2023-08-15 17:58:38 -0700

    move from Mantine to Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { MantineProvider } from "@mantine/core";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
