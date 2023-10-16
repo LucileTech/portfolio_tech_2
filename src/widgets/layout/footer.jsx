c3cf1c646ca42c2381641c6e327d62429e49ffcb
LucileTech <lucile.t.tech@gmail.com>
2023-10-16 15:13:32 -0700

    tailwing template inspo

---
+++ src/widgets/layout/footer.jsx
@@ -0,0 +1,111 @@
+import PropTypes from "prop-types";
+import { Typography, IconButton } from "@material-tailwind/react";
+
+const year = new Date().getFullYear();
+
+export function Footer({ title, description, socials, menus, copyright }) {
+  return (
+    <footer className="relative px-4 pb-6 pt-8">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap pt-6 text-center lg:text-left">
+          {/* <div className="mx-auto mt-12 grid w-max grid-cols-2 gap-24 lg:mt-0"> */}
+          <div className="mx-auto mb-8 mt-6 flex justify-center gap-2 md:mb-0 lg:justify-start">
+            {socials.map(({ color, name, path }) => (
+              <a
+                key={name}
+                href={path}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconButton color="white" className="rounded-full">
+                  <Typography color={color}>
+                    <i className={`fa-brands fa-${name}`} />
+                  </Typography>
+                </IconButton>
+              </a>
+            ))}
+          </div>
+          {/* </div> */}
+        </div>
+        <hr className="my-6 border-gray-300" />
+        <div className="flex flex-wrap items-center justify-center md:justify-between">
+          <div className="mx-auto w-full px-4 text-center">
+            <Typography
+              variant="small"
+              className="font-normal text-blue-gray-500"
+            >
+              {copyright}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.defaultProps = {
+  title: "Material Tailwind",
+  description:
+    "Easy to use React components for Tailwind CSS and Material Design.",
+  socials: [
+    {
+      color: "blue",
+      name: "facebook",
+      path: "https://www.facebook.com/CreativeTim",
+    },
+    {
+      color: "light-blue",
+      name: "twitter",
+      path: "https://www.twitter.com/creativetim",
+    },
+    {
+      color: "purple",
+      name: "instagram",
+      path: "https://www.instagram.com/creativetimofficial/",
+    },
+    {
+      color: "pink",
+      name: "dribbble",
+      path: "https://www.dribbble.com/creativetim",
+    },
+    {
+      color: "red",
+      name: "youtube",
+      path: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
+    },
+    {
+      color: "black",
+      name: "github",
+      path: "https://github.com/creativetimofficial/material-tailwind",
+    },
+  ],
+  menus: [
+    {
+      name: "useful links",
+      items: [
+        { name: "About Us", path: "https://www.creative-tim.com/presentation" },
+        { name: "Blog", path: "https://www.creative-tim.com/blog" },
+        {
+          name: "Github",
+          path: "https://www.github.com/creativetimofficial/material-tailwind?ref=mtk",
+        },
+        {
+          name: "Free Products",
+          path: "https://www.creative-tim.com/templates/free?ref=mtk",
+        },
+      ],
+    },
+  ],
+};
+
+Footer.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  socials: PropTypes.arrayOf(PropTypes.object),
+  menus: PropTypes.arrayOf(PropTypes.object),
+  copyright: PropTypes.node,
+};
+
+Footer.displayName = "/src/widgets/layout/footer.jsx";
+
+export default Footer;
